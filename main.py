14e1606ff5d051c4a17f1c7dc30532bf3c73db11
Jace <JaceCodeMail@gmail.com>
2024-07-13 13:04:27 -0700

    'Checked-in' and 'Never checked-in' timeouts have been combined.

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ pollEvery_thMinute = 10 #Example: 10 = Poll at 3:00, 3:10, 3:20, 3:30, etc. Not 
 
 emailAlertTimeoutHr = 5 # Send an email every x hours until the temperature goes back within bounds.
 
-tagFirstTimeoutMin = 5 # Notify when tag has not checked since script start after x minutes
 tagTimeoutTimeMin = 30 # Notify when tag has not checked in after x minutes
 
 timeoutEmailDelayTimeSec = 60 * 60 * 24 #Timeout emails can be sent only once every 24 hours per RuuviTag
@@ -43,14 +42,22 @@ for mac, cfg in config.tagConfigs.items():
     lastTimeoutEmailDict[mac] = float('-inf')
     ruuviTagDataHandler[mac] = DataHandler(mac, emailAlertTimeoutHr)
 
-def send_timeout_alert(mac, lastCheckinTime, sensorName:str):
-    if time.time() >=  lastTimeoutEmailDict[mac] + timeoutEmailDelayTimeSec:
-        message = f"The greenhouse sensor '{sensorName}' has not sent a signal in {lastCheckinTime:.2f} minutes. Verify it is still within range and the battery is good.\n"
-        message += f"This message will repeat in {(timeoutEmailDelayTimeSec/60/60):.2f} hours if it is not resolved.\n"
-        message += "\n-The Greenhouse Monitor"
-        status = EmailHandler.send_message("Automatic Greenhouse Timeout Alert", message, rxEmails=None, debugOnly=debugMode)
-        if status != None:
-            lastTimeoutEmailDict[mac] = time.time()
+def send_timeout_alert(mac, lastCheckinTimeMin:float, sensorName:str, neverCheckedIn:bool):
+    if time.time() <  lastTimeoutEmailDict[mac] + timeoutEmailDelayTimeSec:
+        return #Prevent email spam when the tag hasn't checked in
+
+    message = ""
+    if neverCheckedIn:
+        message = f"The greenhouse monitor has been on for {lastCheckinTimeMin:.2f} minutes, but the sensor '{sensorName}' has not sent a signal yet. Verify it is still within range and the battery is good.\n"
+    else:
+        message = f"The greenhouse sensor '{sensorName}' has not sent a signal in {lastCheckinTimeMin:.2f} minutes. Verify it is still within range and the battery is good.\n"
+
+    message += f"This message will repeat in {(timeoutEmailDelayTimeSec/60/60):.2f} hours if it is not resolved.\n"
+    print(message)
+    message += "\n-The Greenhouse Monitor"
+    status = EmailHandler.send_message("Automatic Greenhouse Timeout Alert", message, rxEmails=None)
+    if status != None:
+        lastTimeoutEmailDict[mac] = time.time()
 
 def handle_tag_data(tagData):
     recentMacs = list(tagData.keys())
@@ -67,19 +74,16 @@ async def check_tag_timeout():
     for mac, cfg in config.tagConfigs.items():
         if not cfg.enabled:
             continue
-        lastCheckin = await(ruuvi.minutes_since_last_checkin(mac))
-        if lastCheckin != None:
-            if lastCheckin > tagTimeoutTimeMin:
-                print(f"{cfg.name} has not sent a signal in {lastCheckin:.2f} minutes. Verify that it is in range and the battery is still good.")
-                send_timeout_alert(mac, lastCheckin, cfg.name)
-                
-        else:
-            #TODO: Handle case where the tag has not checked in at all.
-            runtimeMins = (time.time() - programStartTime)/60
-            if runtimeMins > tagFirstTimeoutMin:
-                print(f"{cfg.name} has yet to send any signal. Verify that it is in range and the battery is not dead.")
-                print(f"The monitor has been up for {runtimeMins:.2f} minute(s)")
 
+        neverCheckedIn = False
+
+        lastCheckin = await(ruuvi.minutes_since_last_checkin(mac))
+        if lastCheckin == None:
+            neverCheckedIn = True
+            lastCheckin = (time.time() - programStartTime)/60
+
+        if lastCheckin > tagTimeoutTimeMin:
+            send_timeout_alert(mac, lastCheckin, cfg.name, neverCheckedIn)
 
 async def main():
     #TODO: need to find a way to gracefully stop this task while it is stuck waiting for the generator
